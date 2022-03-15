7f6e62e87607e4829fa69f64c2fb77ed164bdc5b
charlesantunes <charleswcantunes@gmail.com>
2022-03-14 22:49:44 -0300

    segundo commit projeto api

--- app-php/src/app/curso/curso.component.ts
+++ app-php/src/app/curso/curso.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso.component.css']  //stylo da página interna
 })
 export class CursoComponent implements OnInit {
+  //nome:string = 'Charles';  //10-testando a variável na página, teste será com o bind{{}} no curso.componente.html, apagar depois
 
+  //Construtor
   constructor() { } //executa alguma funcionalidade ou estancia um objeto
 
+  //inicializador do sistema
   ngOnInit(): void {  //ao iniciar a pagina pode executar uma ação.
+    //alert("Olá, teste de inicialização"); //09-testando a função de inicialização, apagar depois.
+  }
+
+  //12-Cadastro
+  cadastro():void{  //13-função com retorno(void)
+    //alert("Olá, teste de Cadastro") //17-apenas para teste, apagar depois
+  }
+
+  //14-Seleção
+  selecao():void{
+    //alert("Olá, teste de Seleção")  //apenas para teste, apagar depois
+  }
+
+  //15-Alterar
+  alterar():void{
+    //alert("Olá, teste de Alterar")  //apenas para teste, apagar depois
+  }
+  
+  //16-Remover
+  remover():void{
+    //alert("Olá, teste de Remover")  //apenas para teste, apagar depois
   }
 
 }
---
+++ app-php/src/app/curso/curso.ts
@@ -0,0 +1,8 @@
+//19-Classe
+export class Curso {    //Toda classe deve começão com letra maiúscula, poderia ser qualquer nome
+    
+    //20-Construtor - onde passará os parâmetros necessários p/ efetuar alguma ação, seja cadastro, seleção...
+    constructor(nomeCurso: string, valorCurso:number, idCurso?:number){  //21- interrogação é p/ informar que pode ser que não use esse objeto, pq ele é auto incremento no BD
+
+    }
+}
